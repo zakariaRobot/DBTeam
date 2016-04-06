4ada6d9a61d73036482bec9ab10405d4555887bb
zakariaRobot <rasoli.zakaria@gmail.com>
2016-04-06 11:40:53 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -246,7 +246,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {139328010},
     admin_users = {},
     disabled_channels = {}
   }
