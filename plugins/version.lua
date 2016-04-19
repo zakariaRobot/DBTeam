2679598828a93977e6afb8482953e523ec0d2bf1
zakariaRobot <rasoli.zakaria@gmail.com>
2016-04-19 22:13:27 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,7 +14,7 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @MaSkAoS @Josepdal\nSupport: @skneos @iicc1 @Serx666\nChannels: @DBTeamEN @DBTeamES\n\nCheckout: https://github.com/Josepdal/DBTeam\nGNU GPL v2 license.'
+  return 'ربات مدیریت پیشرفته گروه ورژن 5     ادمین اصلی ربات :@ZakariaR'
 end
 
 return {
